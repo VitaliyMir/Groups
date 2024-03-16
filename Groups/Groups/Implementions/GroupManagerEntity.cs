503fb8f1f2137918aa5b72d68c1950f00f9bbec4
Vitalii Myroshnychenko <vitalik2005mir@gmail.com>
2024-03-16 12:46:30 +0100

    Implemented GroupManagerEntity class for managing groups

--- Groups/Groups/Implementions/GroupManagerEntity.cs
+++ Groups/Groups/Implementions/GroupManagerEntity.cs
@@ -4,5 +4,20 @@ namespace Groups.Implementions;
 
 public class GroupManagerEntity
 {
+    public List<GroupEntity> Groups { get; private set; }
 
+    public GroupManagerEntity()
+    {
+        Groups = new List<GroupEntity>();
+    }
+
+    public void AddGroup(GroupEntity group)
+    {
+        Groups.Add(group);
+    }
+
+    public GroupEntity FindGroupByName(string name)
+    {
+        return Groups.FirstOrDefault(g => g.Name == name);
+    }
 }
