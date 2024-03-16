85b5d916f833d265bca1a3aa9a0366c6a6caacc1
Vitalii Myroshnychenko <vitalik2005mir@gmail.com>
2024-03-16 12:53:26 +0100

    Merge feature_person_entity. Created PersonEntity class to represent individuals.

--- Groups/Groups/Implementions/PersonEntity.cs
+++ Groups/Groups/Implementions/PersonEntity.cs
@@ -2,5 +2,12 @@ namespace Groups.Implementions;
 
 public class PersonEntity
 {
+    public string Name { get; set; }
+    public int Age { get; set; }
 
+    public PersonEntity(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
 }
