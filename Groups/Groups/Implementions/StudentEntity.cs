a52e1ddc3de957e13a29c42f5e52b27aaae529fa
Vitalii Myroshnychenko <vitalik2005mir@gmail.com>
2024-03-16 12:57:18 +0100

    Merge feature_student_entity. Added StudentEntity with inheritance from PersonEntity.

--- Groups/Groups/Implementions/StudentEntity.cs
+++ Groups/Groups/Implementions/StudentEntity.cs
@@ -2,5 +2,10 @@ namespace Groups.Implementions;
 
 public class StudentEntity : PersonEntity
 {
+    public int StudentId { get; set; }
 
+    public StudentEntity(string name, int age, int studentId) : base(name, age)
+    {
+        StudentId = studentId;
+    }
 }
