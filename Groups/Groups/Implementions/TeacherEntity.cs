accfc103e6cec37f92da6b82560114e558d3f582
Vitalii Myroshnychenko <vitalik2005mir@gmail.com>
2024-03-16 13:07:41 +0100

    Merge feature_teacher_entity. Expanded PersonEntity into TeacherEntity with subject association.

--- Groups/Groups/Implementions/TeacherEntity.cs
+++ Groups/Groups/Implementions/TeacherEntity.cs
@@ -4,5 +4,15 @@ namespace Students.Implementations;
 
 public class TeacherEntity : PersonEntity
 {
+    public List<SubjectEntity> Subjects { get; set; }
 
+    public TeacherEntity(string name, int age) : base(name, age)
+    {
+        Subjects = new List<SubjectEntity>();
+    }
+
+    public void AddSubject(SubjectEntity subject)
+    {
+        Subjects.Add(subject);
+    }
 }
